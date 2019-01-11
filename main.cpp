2add060e193fc9d139098bc7db33138edc03cacc
h-summit <45748016+h-summit@users.noreply.github.com>
2019-01-11 14:14:39 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <fstream>
+#include"Naive_Bayes.h"
+using namespace std;
+
+int main()
+{ 
+	Naive_Bayes a(58);//参数加上结果共58个
+	a.train("train.data");//训练集
+	a.test("test.data");//测试集
+}
